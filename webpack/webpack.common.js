eb04bd7764d9c63d3aa90d31b3209b08a8ee0eee
Jaehyun An <uwoobeat@gmail.com>
2025-11-23 17:28:04 +0900

    chore: tailwindcss 및 postcss 관련 세팅

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -29,10 +29,21 @@ module.exports = {
                 use: "ts-loader",
                 exclude: /node_modules/,
             },
+            {
+                test: /\.css$/,
+                use: [
+                    "style-loader",
+                    "css-loader",
+                    "postcss-loader",
+                ],
+            },
         ],
     },
     resolve: {
         extensions: [".ts", ".tsx", ".js"],
+        alias: {
+            "@": path.join(__dirname, "..", "src"),
+        },
     },
     plugins: [
         new CopyPlugin({
